ff799fb154e42079dc48ef29828fc58b7cd3015a
Selena Mora <selenacardenaas1023@gmail.com>
2026-02-18 14:12:08 -0500

    proyecto turnos

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Página principal
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Ruta dinámica (ejemplo: reserva de turno)
-@app.route('/reserva/<cliente>')
-def reserva(cliente):
-    return f"Bienvenido, {cliente}. Tu reserva está confirmada para hoy."
+@app.route('/about')
+def about():
+    return render_template('about.html')  # luego creas about.html
 
 if __name__ == '__main__':
     app.run(debug=True)
